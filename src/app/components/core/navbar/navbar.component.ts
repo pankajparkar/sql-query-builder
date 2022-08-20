ab98346fb6a7414f8d0b7390b5d7b271750c7329
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-08-20 15:46:43 +0800

    Layout for query-builder.

--- src/app/components/core/navbar/navbar.component.ts
+++ src/app/components/core/navbar/navbar.component.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 const imports = [
   CommonModule,
   MatButtonModule,
   MatToolbarModule,
   MatIconModule,
+  RouterModule,
 ];
 
 @Component({
--- src/app/components/query-builder/query-builder.component.ts
+++ src/app/components/query-builder/query-builder.component.ts
@@ -1,18 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+
+import { TableViewComponent } from '../table-view/table-view.component';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+const imports = [
+  CommonModule,
+  ReactiveFormsModule,
+  MatButtonModule,
+  MatCardModule,
+  MatDividerModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatInputModule,
+
+  // components
+  TableViewComponent,
+]
 
 @Component({
   selector: 'sqb-query-builder',
   standalone: true,
-  imports: [CommonModule],
+  imports,
   templateUrl: './query-builder.component.html',
   styleUrls: ['./query-builder.component.scss']
 })
-export class QueryBuilderComponent implements OnInit {
+export class QueryBuilderComponent {
+  toppings = new FormControl('');
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato',];
 }
