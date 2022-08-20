ab082e4c928e97c792ef93402cbb8bc4785b981f
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-08-20 22:44:59 +0800

    Removed unwanted code. Use MatDatasource instead of other thing.

--- src/app/components/core/table/table.component.ts
+++ src/app/components/core/table/table.component.ts
@@ -5,48 +5,6 @@ import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { Column } from 'src/app/models/column.model';
 
-
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  fruit: string;
-}
-
-/** Constants used to fill up our data base. */
-const FRUITS: string[] = [
-  'blueberry',
-  'lychee',
-  'kiwi',
-  'mango',
-  'peach',
-  'lime',
-  'pomegranate',
-  'pineapple',
-];
-
-const NAMES: string[] = [
-  'Maia',
-  'Asher',
-  'Olivia',
-  'Atticus',
-  'Amelia',
-  'Jack',
-  'Charlotte',
-  'Theodore',
-  'Isla',
-  'Oliver',
-  'Isabella',
-  'Jasper',
-  'Cora',
-  'Levi',
-  'Violet',
-  'Arthur',
-  'Mia',
-  'Thomas',
-  'Elizabeth',
-];
-
 const imports = [
   CommonModule,
   MatTableModule,
@@ -68,18 +26,10 @@ export class TableComponent implements AfterViewInit {
   @Input()
   columns: Column[] = [];
 
-  dataSource: MatTableDataSource<UserData>;
+  dataSource: MatTableDataSource<any[]> = new MatTableDataSource();
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-
-  constructor() {
-    // Create 100 users
-    const users = Array.from({ length: 100 }, (_, k) => createNewUser(k + 1));
-
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
-  }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
@@ -101,25 +51,12 @@ export class TableComponent implements AfterViewInit {
 
   ngOnChanges(changes: SimpleChanges) {
     const columns = changes['columns'];
+    const tableData = changes['tableData'];
     if (this.simpleChangeCheck(columns)) {
-      this.columns = columns.currentValue ?? [];
       this.displayedColumns = this.columns.map(col => col.colName);
     }
+    if (this.simpleChangeCheck(tableData)) {
+      this.dataSource = new MatTableDataSource(tableData.currentValue);
+    }
   }
-}
-
-/** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name =
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
-    ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
-    '.';
-
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
-  };
 }
