ef452748bd5185c5607af1923cbb5bb639c2f8c7
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-08-20 16:28:20 +0800

    Added enum and JSON called based on if condition.

---
+++ src/app/enums/table-name.enum.ts
@@ -0,0 +1,10 @@
+export enum TableName {
+    Category = 'category',
+    Customer = 'customer',
+    Employee = 'employee',
+    Order = 'order',
+    Product = 'product',
+    Region = 'region',
+    Shipper = 'shipper',
+    Supplier = 'supplier',
+}
--- src/app/services/query.service.ts
+++ src/app/services/query.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable, of } from 'rxjs';
+import { TableName } from '../enums/table-name.enum';
 import { Column } from '../models/column.model';
 import { TableData } from '../models/table-data.model';
 
@@ -19,11 +20,32 @@ export class QueryService {
   }
 
   getData(query: string): Observable<TableData> {
-    const table = query.includes('test');
     let apiCall;
+    // TODO: simplified switch case by having simple if condition
     switch (query) {
-      case 'categories':
-        apiCall = this.http.get('/assets/categories.json');
+      case TableName.Category:
+        apiCall = this.http.get('/assets/category.json');
+        break;
+      case TableName.Customer:
+        apiCall = this.http.get('/assets/customer.json');
+        break;
+      case TableName.Employee:
+        apiCall = this.http.get('/assets/employee.json');
+        break;
+      case TableName.Order:
+        apiCall = this.http.get('/assets/order.json');
+        break;
+      case TableName.Product:
+        apiCall = this.http.get('/assets/product.json');
+        break;
+      case TableName.Region:
+        apiCall = this.http.get('/assets/region.json');
+        break;
+      case TableName.Shipper:
+        apiCall = this.http.get('/assets/shipper.json');
+        break;
+      case TableName.Supplier:
+        apiCall = this.http.get('/assets/supplier.json');
         break;
       default:
         return of<TableData>({
